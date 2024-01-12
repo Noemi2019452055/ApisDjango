9fd5e73b8a6513c6e9d47fbd4abdc8ea617f9f09
Noemi2019452055 <2019452055@teschi.edu.mx>
2024-01-11 20:18:36 -0600

    imagenes y boton paypai

--- api/models.py
+++ api/models.py
@@ -18,3 +18,43 @@ class Respuestaschatbot(models.Model):
 
     def str(self):
         return str(self.nombre_completo)
+    
+    
+##########################################################################################
+        #pruebas de testing
+        # models.pyS
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=15)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad_stock = models.PositiveIntegerField()
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha_venta = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Venta {self.id} - Cliente: {self.cliente.nombre}"
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"DetalleVenta {self.id} - Venta: {self.venta.id}, Producto: {self.producto.nombre}"
